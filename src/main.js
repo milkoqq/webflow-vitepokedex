e0ab08ab27cd2cde94ab2eaf2980758508c2c1a0
milkoqq <theofanous.dim@gmail.com>
2022-07-07 17:58:16 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,10 +13,16 @@ const pokedexDetector = document.querySelector('.pokedex_detector')
 const containerBtnsBlue = document.querySelector('#containerBtnsBlue')
 const btnNum = document.querySelector('.poke_right-btn-blue')
 const btnNums = Array.from(document.querySelectorAll('.poke_right-btn-blue')) //nodelist doesn't have indexof 
+const btnRandom = document.querySelector('.pokedex_random-btn')
+const btnUp = document.querySelector('#box_cross-top')
+const btnBottom = document.querySelector('#box_cross-bottom')
+const btnLeft = document.querySelector('#box_cross-left')
+const btnRight = document.querySelector('#box_cross-right')
 
 
 let pokemon; // Global Pokemon Object
 let pokemonDesc;
+let pokemonType = ''
 let lights;
 let lastPokemon = '898' // probably, for now.
 let numPokemon = ''; // Global Pokemon Number
@@ -44,7 +50,8 @@ async function getPokemon(id) {
     try {
         pokemon = await getJSON(`https://pokeapi.co/api/v2/pokemon/${id}`, `Couldn't Fetch Pokemon`)
         let pokemonSpecies = await getJSON(`https://pokeapi.co/api/v2/pokemon-species/${id}`, `Couldn't Fetch Description`)
-        pokemonDesc = pokemonSpecies.flavor_text_entries[0].flavor_text
+        let pokemonInEn = pokemonSpecies.flavor_text_entries.find(entry => entry.language.name === 'en')
+        pokemonDesc = pokemonInEn.flavor_text
         updateUI()
     }
     catch (e) {
@@ -60,6 +67,7 @@ function updateUI() {
     labelPokemonName.textContent = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
     labelPokemonCounter.classList.remove('hide')
     labelPokemonCounter.textContent = id.toString().padStart(3, '0')
+    imgPokemonType.src = setAsset(pokemon.types[0].type.name)
     typeWrite(String(pokemonDesc), 15, labelPokemonDesc)
     setLights()
 
@@ -92,12 +100,15 @@ btnNums.forEach(btn => {
 
 containerBtnsBlue.addEventListener('click', (e) => {
     if (varTypeWrite) return
+    debugger;
+    console.log(`NumPokemon`, numPokemon)
     numPokemon += e.target.dataset.value
+    console.log(`NumPokemon`, numPokemon)
     labelPokemonDesc.textContent = ''
     labelPokemonDesc.textContent = `Seeking pokémon...... ${numPokemon}`
     wait(2)
         .then(() => {
-            console.log(numPokemon)
+            console.log(`NumPokemon`, numPokemon)
             console.log(numPokemon.length)
             if (numPokemon.length > 3) {
                 labelPokemonDesc.textContent = 'Pokémon Not Found...... Try again...'
@@ -113,7 +124,7 @@ containerBtnsBlue.addEventListener('click', (e) => {
 })
 
 
-
+//Typewriting Effect. Can be used anywhere. Variable sets the state, so nothing else is clickable while functioning. 
 function typeWrite(str, speed, element) {
     varTypeWrite = true
     element.textContent = ''
@@ -129,5 +140,61 @@ function typeWrite(str, speed, element) {
     typer()
 }
 
+function setAsset(type) {
+    switch (type) {
+        case 'normal':
+            return 'https://cdn2.bulbagarden.net/upload/3/39/NormalIC_Big.png';
+        case 'fighting':
+            return 'https://cdn2.bulbagarden.net/upload/6/67/FightingIC_Big.png';
+        case 'flying':
+            return 'https://cdn2.bulbagarden.net/upload/c/cb/FlyingIC_Big.png';
+        case 'poison':
+            return 'https://cdn2.bulbagarden.net/upload/3/3d/PoisonIC_Big.png';
+        case 'ground':
+            return 'https://cdn2.bulbagarden.net/upload/8/8f/GroundIC_Big.png';
+        case 'rock':
+            return 'https://cdn2.bulbagarden.net/upload/c/ce/RockIC_Big.png';
+        case 'bug':
+            return 'https://cdn2.bulbagarden.net/upload/c/c8/BugIC_Big.png';
+        case 'ghost':
+            return 'https://cdn2.bulbagarden.net/upload/7/73/GhostIC_Big.png';
+        case 'steel':
+            return 'https://cdn2.bulbagarden.net/upload/d/d4/SteelIC_Big.png';
+        case 'fire':
+            return 'https://cdn2.bulbagarden.net/upload/2/26/FireIC_Big.png';
+        case 'water':
+            return 'https://cdn2.bulbagarden.net/upload/5/56/WaterIC_Big.png';
+        case 'grass':
+            return 'https://cdn2.bulbagarden.net/upload/7/74/GrassIC_Big.png';
+        case 'electric':
+            return 'https://cdn2.bulbagarden.net/upload/4/4a/ElectricIC_Big.png';
+        case 'psychic':
+            return 'https://cdn2.bulbagarden.net/upload/6/60/PsychicIC_Big.png';
+        case 'ice':
+            return 'https://cdn2.bulbagarden.net/upload/6/6f/IceIC_Big.png';
+        case 'dragon':
+            return 'https://cdn2.bulbagarden.net/upload/4/48/DragonIC_Big.png';
+        case 'dark':
+            return 'https://cdn2.bulbagarden.net/upload/5/56/DarkIC_Big.png';
+        case 'fairy':
+            return 'https://cdn2.bulbagarden.net/upload/d/df/Picross_FairyIC.png';
+        default:
+            return 'https://cdn2.bulbagarden.net/upload/3/3c/UnknownIC_Big.png';
+    }
+}
+
+btnRandom.addEventListener('click', () => {
+    getPokemon(randomPokemon(0, 889))
+
+})
+
+function randomPokemon(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// btnUp.addEventListener('click', () => {
+//     getPokemon(numPokemon--)
+// })
 init()
 
