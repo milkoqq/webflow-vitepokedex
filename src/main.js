ca98e3ea8b18610cc0be0c289780b1492764cdff
milkoqq <theofanous.dim@gmail.com>
2022-07-07 22:03:57 +0300

    Fixed bug, fetching more than 1. Added Cross btns

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ let varTypeWrite = false // Global state of typewriting. Allows for typewriting 
 // Stuff that should happen on initialization.
 
 function init() {
-    wait(4)
+    wait(3)
         .then(() => {
             typeWrite('Hello there! Welcome to the world of pokémon!💖 My name is Prof. Oak! This is your pokédex a tool to find any pokémon..', 38, labelPokemonDesc)
         })
